5c16dba98b0e81ed8d2db926fde4962ad7ed0b7e
Rachit Patel <rachitp509@gmail.com>
2024-08-24 15:41:49 +0530

    added server url

--- src/utils/axiosUtils.jsx
+++ src/utils/axiosUtils.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://z-pay-server.adaptable.app",
 });
 
 export default axiosInstance;
